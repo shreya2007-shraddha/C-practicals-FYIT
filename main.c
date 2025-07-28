b1cd0a427a8caa52d563390d7ca5a7022511f582
shreya2007-shraddha <shreyagadge093@gmail.com>
2025-07-28 14:41:40 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 
 int main()
-{
-    printf("Hello world!\n");
+{float p,r,t,si;
+
+    printf("enter principal ammount:\n");
+    scanf("%f",&p);
+
+    printf("enter rate of interest:\n");
+    scanf("%f",&r);
+
+    printf("enter period of time (in years):\n");
+    scanf("%f",&t);
+
+    si = (p*r*t) / 100;
+
+    printf("simple intrest = %.2f \n" , si);
+
     return 0;
 }
